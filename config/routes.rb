eef12a93a3dbabfb7aaee30bb4f082a6510dea5a
Takao <k.takao@neogenia.co.jp>
2022-05-01 14:07:33 +0900

    feat: add api route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # @example https://localhost:3000/api/v1/restaurants/
+  namespace :api do
+    namespace :v1 do
+      resources :restaurants do
+        # example https://localhost:3000/api/v1/restaurants/1/foods
+        resources :foods, only: %i[index]
+      end
+
+      resources :line_foods, only: %i[index create]
+      put 'line_foods/replace', to: 'line_foods#replace'
+
+      resources :orders, only: %i[create]
+
+    end
+  end
 end
