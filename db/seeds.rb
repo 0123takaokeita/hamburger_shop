a9eb3a0d353440a7178d218eb2cf7d11a143e03e
Takao <k.takao@neogenia.co.jp>
2022-05-01 14:27:41 +0900

    feat: add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+3.times do |i|
+  restaurant = Restaurant.new(
+    name: "testレストラン_#{i+1}",
+    fee: rand(100..500),
+    time_required: 10,
+  )
+
+  12.times do |i|
+    restaurant.foods.build(
+      name: "フード名_#{i+1}",
+      price: 500,
+      description: "フード_#{i+1}の説明文です。"
+    )
+  end
+
+  restaurant.save!
+end
