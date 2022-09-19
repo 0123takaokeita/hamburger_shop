05044a8a78a343ae66dff00d47ad7109d802870d
Takao <k.takao@neogenia.co.jp>
2022-09-19 14:48:55 +0900

    feat: toppage にrestaurants 一覧表示

--- front/src/containers/Restaurants.jsx
+++ front/src/containers/Restaurants.jsx
@@ -1,6 +1,7 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useReducer, useEffect } from 'react';
 import styled from 'styled-components';
-import {fetchRestaurants} from '../apis/restaurants';
+import { fetchRestaurants } from '../apis/restaurants';
+import { initialState, restaurantsActionTyps, restaurantsReducer, } from '../reducers/restaurants';
 import MainLogo from '../images/logo.png';
 import MainCoverImage from '../images/main-cover-image.png';
 
@@ -8,33 +9,46 @@ const HeaderWrapper = styled.div`
     display: flex;
     justify-content: flex-start;
     padding: 8px 32px;
-`
+`;
 
 const MainCoverImageWrapper = styled.div`
     text-align: center;
-`
+`;
 
 const MainLogoImage = styled.img`
     height: 90px;
 `
-
 const MainCover = styled.img`
     height: 600px;
-`
+`;
+
 export const Restaurants = () => {
+    const [state, dispatch] = useReducer(restaurantsReducer, initialState);
+
     useEffect(() => {
+        dispatch({ type: restaurantsActionTyps.FETCHING });
         fetchRestaurants()
-        .then((data) => console.log(data))
+        .then((data) =>
+            dispatch({
+                type: restaurantsActionTyps.FETCH_SUCCESS,
+                payload: { restaurants: data.restaurants }
+            })
+        )
     }, [])
 
     return (
-        <Fragment>
-            <HeaderWrapper>
-                <MainLogoImage src={MainLogo} alt='main logo' />
-            </HeaderWrapper>
-            <MainCoverImageWrapper>
-                <MainCover src={MainCoverImage} alt='main cover' />
-            </MainCoverImageWrapper>
-        </Fragment>
+    <Fragment>
+        <HeaderWrapper>
+            <MainLogoImage src={MainLogo} alt="main logo" />
+        </HeaderWrapper>
+        <MainCoverImageWrapper>
+            <MainCover src={MainCoverImage} alt="main cover" />
+        </MainCoverImageWrapper>
+        {
+            state.restaurantsList.map(restaurant =>
+                <div> {restaurant.name} </div>
+            )
+        }
+    </Fragment>
     )
 }
